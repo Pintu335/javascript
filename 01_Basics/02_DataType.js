a4b69d772d988275e70d88d7012ce7560287a8c9
Pintu335 <114636285+Pintu335@users.noreply.github.com>
2024-02-13 03:35:12 +0000

    commit

---
+++ 01_Basics/02_DataType.js
@@ -0,0 +1,17 @@
+"use strict";// treat all the JS code as newer version 
+//alert(3+3) // we are using nodejs, not browser
+console.log(3+3)
+/* 
+data types in javaScript
+1= number => 2 to power 53
+2= bigint
+3= String=>""
+4= boolean=> true/false
+5= null=> standalone value
+6= undefined=> undefined
+7= symbol => unique
+    
+*/
+// typeof is used to check the data type of the variable
+console.log(typeof undefined);  // it will give the output as the undefined
+console.log(typeof null);// it will give the output as object 
