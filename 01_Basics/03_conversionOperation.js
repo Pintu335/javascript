68efc8034e2480931dffef0da89431aefb5e84d2
Pintu335 <114636285+Pintu335@users.noreply.github.com>
2024-02-14 02:49:06 +0000

    operation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -24,6 +24,28 @@ let xOutput=Number(x);
 
 let y=""
 let output=Boolean(y);
-console.log(output);          
+//console.log(output);          
 // if we give sting value empty (" ") and convert it into the boolean datatype the output 
 //will be false and if string contain some letter then  output will be true 
+
+
+//.......................................................opertions.............................................
+let num=1;
+let NumValue=-num;
+// console.log(NumValue);
+ // console.log(2+1);
+ // console.log(2-1);
+ // console.log(2/1);
+ // console.log(2*1);
+ // console.log(2**1); power
+ // console.log(2%1);
+ 
+console.log(1+1+"1");// here the convesion take place depending upond the first letter output=>21
+console.log("1"+2+2); // the output is 122 because the first letter is string that why reset 2 number will not be added
+let str1=" hello "
+let str2="  Pintu"
+let str3=str1+str2;
+console.log(str3);
+let z=true
+console.log(+z);// here the value of +z is converted into number so the output becomes 1
+console.log(+"");// here the value of "" is converted into number so the output becomes zero
