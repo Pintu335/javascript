6b92f3941ce77a866d0e1bdbe00a0239a1beafaa
Pintu335 <114636285+Pintu335@users.noreply.github.com>
2024-02-17 18:44:10 +0000

    comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,20 @@
+console.log(2<1);
+console.log(2>1);
+console.log(1==1);
+
+console.log("2">1) ; // output true because there the type convesion is done so "2" is converted into number
+console.log("02"> 1);
+
+// we should avoid below comparisions
+console.log(null > 0);// out put false
+
+console.log(null==0); // output false
+console.log(null>=0);//output true;
+console.log(undefined==0);// output false;
+
+console.log(undefined>0);// output false;
+
+console.log(undefined<0);// output false;
+// for the strict comparision we use === which do not do the type conversion
+console.log(1===1);// output true
+console.log("2">1)// output false;
