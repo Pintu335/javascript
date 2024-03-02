c9bd3c25a1bea45d9447152633bf72419e7fdc59
Pintu335 <114636285+Pintu335@users.noreply.github.com>
2024-03-02 05:13:32 +0000

    1

---
+++ control_flow/one.js
@@ -0,0 +1,20 @@
+// const temp=41;
+// if(temp==42){
+//     console.log("this is true")
+// }
+// else{
+//     console.log("this is not true");
+// }
+// console.log("execution done");
+
+// const score=200;
+// if(score>100){
+//     const power="fly";   
+//     console.log(`user power: ${power}`);
+// }
+
+// const balance=100;
+// if(balance>50) console.log("true");
+
+
+// use nested for loop and use and operator and or operator and so on
